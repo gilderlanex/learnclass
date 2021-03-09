be4bf08dcf7eb8d18689c8b72f8eafd9ef9f4c5d
Gilderlane Xavier <gilderlanesilva@gmail.com>
2021-03-09 19:23:07 -0400

    adicionado novo atrib e método  a classe pessoa

--- pessoa.py
+++ pessoa.py
@@ -1,7 +1,8 @@
 class Pessoa:
-    def __init__(self, nome, dtnascimento):
+    def __init__(self, nome, dtnascimento, end):
         self.nome = nome
         self.dtnascimento = dtnascimento
+        self.end = end
 
     def setNome(self, nome): # Método para modificar.
         self.nome = nome
@@ -9,8 +10,14 @@ class Pessoa:
     def setDtnascimento(self, dtnascimento):
         self.dtnascimento = dtnascimento
 
+    def setend(self, end):
+        self.end = end
+
     def getNome(self): ## Método para capturar.
        return self.nome
 
     def getDtnascimento(self):
         return self.dtnascimento
+
+    def getEndereco(self):
+        return self.end
